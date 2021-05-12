551bcb583ade68368195be63f850bce84145594b
yididia seifu belay <68819154+jedi116@users.noreply.github.com>
2021-05-12 11:46:03 +0200

    Add files via upload

---
+++ fullstack_point72/routes/user.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+// import controller
+const { requireSignin, adminMiddleware } = require('../controllers/auth.controller');
+const { readController, updateController, readAllController } = require('../controllers/user.controller');
+
+router.get('/user/:id', requireSignin, readController);
+router.get('/users', requireSignin, readAllController);
+router.put('/user/update', requireSignin, updateController);
+router.put('/admin/update', requireSignin, adminMiddleware, updateController);
+
+module.exports = router;
